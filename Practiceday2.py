1c81c3f6cbd64aa43448d1412f26b70a70a89c80
DaiSwap <154792629+DaiSwap@users.noreply.github.com>
2024-08-24 22:23:27 +0530

    Each person payment with tip

--- Practiceday2.py
+++ Practiceday2.py
@@ -1,9 +1,14 @@
-'''
+
 #Tipcalculator
 print("let us find out how much tip you owe!")
-A = input("what was the total bill\n")
-print("How much tip would you like to give?")
+A = int(input("what was the total bill\n"))
+B = int(input("How much tip would you like to give? 10, 12 or 15\n"))
+C = int(input("how many people to split the bill?\n"))
 
+D = round(((A*(B/100))+A)/C,2)
+#D = (A*(B/100))/C
+print(f"Each person should pay:{D} rupees")
+'''
 # subscripting
 print("Hello"[0])
 print("Hello"[-1])
@@ -30,10 +35,11 @@ print(5/10)
 print(type(5/10))# implicit type casting
 print(5//10)
 print(2**2)# ** - exponent
-'''
+
 #f-strings - print(f""")
 score = 2
 height = 5.3
 is_winning = False
 
 print(f"Your score is = {score} and your height is {height} . You win might be {is_winning}")
+'''
