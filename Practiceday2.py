549afffd71a0a89602790433103f3cd9ff843f18
DaiSwap <154792629+DaiSwap@users.noreply.github.com>
2024-08-24 21:32:33 +0530

    math op basics

--- Practiceday2.py
+++ Practiceday2.py
@@ -1,5 +1,6 @@
+'''
 #Tipcalculator
-'''print("let us find out how much tip you owe!")
+print("let us find out how much tip you owe!")
 A = input("what was the total bill\n")
 print("How much tip would you like to give?")
 
@@ -15,14 +16,18 @@ print(False)
 
 print(type(False))#to return the type of dataset
 print(len("hello"))# return only length of string and not int/float
-'''
+
 #type conversion
 print(int("123")+int("34")) #str to int using int()
 
-#Eprint("Number of letters in your name:\n" , len(input("enter your name\n")))
+#ERROR - print("Number of letters in your name:\n" + len(input("enter your name\n")))
 
 A = len(input("Enter your name\n"))
 print("Number of letters in your name is:" + str(A)) # Did type conversion here
-
-
+'''
+# math op basic- PEMDASL/BODMAS
+print(5/10)
+print(type(5/10))# implicit type casting
+print(5//10)
+print(2**2)# ** - exponent
 
