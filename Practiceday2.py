a8cbd50fc6a2a4d4681841001d84d4ab0168ab08
DaiSwap <154792629+DaiSwap@users.noreply.github.com>
2024-08-24 21:16:29 +0530

    Type error , type checking & Type conversion

--- Practiceday2.py
+++ Practiceday2.py
@@ -2,8 +2,27 @@
 '''print("let us find out how much tip you owe!")
 A = input("what was the total bill\n")
 print("How much tip would you like to give?")
-'''
+
 # subscripting
 print("Hello"[0])
 print("Hello"[-1])
 print(123_345_456) #LArge integer,  _ just for reading puerpose , makes no changes
+print(56.8765) #Float data type & Floating point number 
+
+#Boolean values 
+print(True)
+print(False)
+
+print(type(False))#to return the type of dataset
+print(len("hello"))# return only length of string and not int/float
+'''
+#type conversion
+print(int("123")+int("34")) #str to int using int()
+
+#Eprint("Number of letters in your name:\n" , len(input("enter your name\n")))
+
+A = len(input("Enter your name\n"))
+print("Number of letters in your name is:" + str(A)) # Did type conversion here
+
+
+
