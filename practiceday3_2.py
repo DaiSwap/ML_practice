f32fb4798045bd98f8618051e3b1439e77aac539
DaiSwap <154792629+DaiSwap@users.noreply.github.com>
2024-08-25 11:46:13 +0530

    Somple conditional statements practice

---
+++ practiceday3_2.py
@@ -0,0 +1,36 @@
+# Conditional state/ logical operators& scope usage
+
+print("Welcome to Treasure Island.\n Your mission is to find the Treasure.\n You're at the crossroad.\n")
+A = int(input("Do you want to go left or right? choose 0 for left and 1 for right"))
+if A == 0:
+    print("you have aarrived at a lake\n")
+    B = input("choose whether to wait for boat or swim, choose 0 to wait,1 to swim")
+    if B == 0:
+        print("you reached")
+    else:
+        print("you died")
+    
+elif A == 1:
+    print("you have been attacked0")
+else:
+    print("your journey ends here.Did not follow the instructions.")
+'''
+# simple if,nested if
+height = int(input("tell your height in cm\n"))
+if height>=120:
+    age = int(input("how old are you?\n"))
+    if age >= 18:
+        print("it will be 10 bucks")
+    else:
+        print("it will be 5 bucks")   
+else:
+    print("not eligible")
+
+
+#check odd or even
+A = int(input("enter any number"))
+if (A%2) == 0:
+    print("the number is even")
+else:
+    print("the number is odd")
+'''
