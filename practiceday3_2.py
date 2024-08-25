46ee435149ed4b0c3f7b58f7d48a960bf9461e73
DaiSwap <154792629+DaiSwap@users.noreply.github.com>
2024-08-25 12:49:07 +0530

    multiple if statements in succession

--- practiceday3_2.py
+++ practiceday3_2.py
@@ -14,28 +14,39 @@ elif A == 1:
     print("you have been attacked0")
 else:
     print("your journey ends here.Did not follow the instructions.")
-
+'''
 # simple if,nested if
 height = int(input("tell your height in cm\n"))
+bill = 0
 if height>=120:
     age = int(input("how old are you?\n"))
     if age <= 12:
+        bill =  10
         print("it will be 10 bucks")
     elif age <= 18:
+        bill = 15
         print("it will be 15 bucks")
     else:
+        bill = 20
         print("it will be 20 bucks")   
+    want_photo = input("Do you want a photo? type Y or N\n")
+    if want_photo == "Y":
+        # add 3 bucks to theyre bill
+        bill += 3 # same as bill = bill + 3
+        print(f"your final bill is {bill}")
+    print("Your final bill will be:",bill)            
 else:
     print("not eligible")
 
-
+'''
 #check odd or even
 A = int(input("enter any number"))
 if (A%2) == 0:
     print("the number is even")
 else:
     print("the number is odd")
-'''
+
+#bmi with conditional statements
 weight = float(input("enter your weight in kg\n"))
 height = float(input("enter your height in metres\n"))
 
@@ -47,5 +58,6 @@ elif bmi>=18.5:
     print("Normal")
 else:
     print("Underweight")
-        
+'''
+     
         
