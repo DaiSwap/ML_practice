0f3d464ccf41a3254f27adbd32c69fe7604bf918
DaiSwap <154792629+DaiSwap@users.noreply.github.com>
2024-08-27 00:03:43 +0530

    good interesting question , used count().

---
+++ task4.py
@@ -0,0 +1,33 @@
+#love calculator
+print("The Love Calculator is calculating your score...")
+name1 = input("enter your name").lower()
+name2 = input("enter your name").lower()
+t1 = name1.count("t")
+r1 = name1.count("r")
+u1 = name1.count("u")
+e1 = name1.count("e")
+t2 = name2.count("t")
+r2 = name2.count("r")
+u2 = name2.count("u")
+e2 = name2.count("e")
+
+l1 = name1.count("l")
+l2 = name2.count("l")
+o1 = name1.count("o")
+o2 = name2.count("o")
+v1 = name1.count("v")
+v2 = name2.count("v")
+ee1 = name1.count("e")
+ee2 = name2.count("e")
+total_true = t1+t2+r1+r2+u1+u2+e1+e2
+total_love = l1+l2+o1+o2+v1+v2+ee1+ee2
+t1 = str(total_true)
+t2 = str(total_love)
+score = t1+t2
+total = int(score)
+if total < 10 or total > 90:
+    print(f"Your score is {total}, you go together like coke and mentos.")
+elif total>=40 and total<=50 :
+    print(f"your score is {total}, you are alright together.")
+else:
+    print(f"your score is {total}")
