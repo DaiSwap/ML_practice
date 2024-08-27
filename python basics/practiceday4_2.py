33eec62e351fc671ca1c46b65fc52b16b54c340d
DaiSwap <154792629+DaiSwap@users.noreply.github.com>
2024-08-28 00:26:48 +0530

    common list index out of range error & nested lists

---
+++ python basics/practiceday4_2.py
@@ -0,0 +1,13 @@
+# most common error of lists - list index out of range
+
+letters = ["a","w","r","y","u","r"] # if index off by one
+ 
+num_of_list = len(letters)
+print(num_of_list)
+print(letters[num_of_list])
+# hence do - print(list[num_of_list -1 ]) as letters["inside is index value"]
+
+# nested list - list inside a list
+blist = ["e","r","i","c","h"]
+nested = [letters,blist]
+print(nested)
