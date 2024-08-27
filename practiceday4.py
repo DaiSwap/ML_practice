bfa0a61951ec58e1a909e890e38c47712da3a4de
DaiSwap <154792629+DaiSwap@users.noreply.github.com>
2024-08-27 23:40:07 +0530

    randomization learing

--- practiceday4.py
+++ practiceday4.py
@@ -2,3 +2,18 @@ import random
 #import my_module
 #print(my_module.my_favnumber)
 
+# using randomization
+'''
+random_integer = random.randint(1,10)
+random_number_0_to_1 = random.random() * 10
+random_float = random.uniform(1,10)
+'''
+
+#randomly print heads or tails
+random_coin = random.randint(0,1)
+if random_coin == 0:
+    print("heads")
+else:
+    print("tails")
+    
+    
