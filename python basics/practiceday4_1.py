1495eb95091379377e241587ac0cfe9a322dda0f
DaiSwap <154792629+DaiSwap@users.noreply.github.com>
2024-08-27 23:59:09 +0530

    lists basics with documentation

---
+++ python basics/practiceday4_1.py
@@ -0,0 +1,14 @@
+#lists
+states = ["karnataka","indianapolis", "delaware"]
+print(states[0])
+print(states[-1])
+
+states[1] = "maharastra"
+print(states[1])
+print(states)
+
+states.append("new jersey")# append at the end of list
+states.extend(["goa","california"])
+print(states)
+
+
