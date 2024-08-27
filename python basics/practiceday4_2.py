ab211ed1834c89392e794a3e46e64447dd23c4ff
DaiSwap <154792629+DaiSwap@users.noreply.github.com>
2024-08-28 00:30:00 +0530

    index usage in nested

--- python basics/practiceday4_2.py
+++ python basics/practiceday4_2.py
@@ -4,10 +4,10 @@ letters = ["a","w","r","y","u","r"] # if index off by one
  
 num_of_list = len(letters)
 print(num_of_list)
-print(letters[num_of_list])
+print(letters[num_of_list - 1])
 # hence do - print(list[num_of_list -1 ]) as letters["inside is index value"]
 
 # nested list - list inside a list
 blist = ["e","r","i","c","h"]
 nested = [letters,blist]
-print(nested)
+print(nested[1])
