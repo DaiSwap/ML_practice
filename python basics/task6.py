d60c5f066a897cfcaccbd2b644b8226ca1c4c69e
DaiSwap <154792629+DaiSwap@users.noreply.github.com>
2024-08-29 19:12:01 +0530

    proper task involving list , length & random solved

--- python basics/task6.py
+++ python basics/task6.py
@@ -1,9 +1,10 @@
 #rock paper scissors game
 import random
+'''
 user_input = int(input("what do you choose? type 0 for rock, 1 for paper or 2 for scissors\n"))
 list = ["rock","paper","scissors"]
 computer = random.randint(0,2)
-print(computer)
+print("computer chose",computer)
 if computer == 0:
     if user_input ==0:
         print("tie")
@@ -25,4 +26,16 @@ if computer == 2:
         print("comp wins")
     else:
         print("tie")
+'''
+#after seeing the solution
+# similar - usage of lists and random.
+# another question - banker roulette
+names = ["angela",'ben','jenny','michael','chloe']
+length = len(names)
+#print(length)
+paybill = random.randint(0,length-1)#length -1 very imp ,finally got the error , all test case passed
+finalbill = names[paybill]
+print(f"{finalbill} is going to buy meal today")
+        
+        
                         
