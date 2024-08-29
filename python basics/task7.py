f7970c6a5e0a6067824ec84f065c9bc01e69d391
DaiSwap <154792629+DaiSwap@users.noreply.github.com>
2024-08-29 23:16:55 +0530

    new task question solving - treasure hiding

---
+++ python basics/task7.py
@@ -0,0 +1,9 @@
+column =["A" , "B" , "C"]
+line1 = ["⬜️","️⬜️","️⬜️"]
+line2 = ["⬜️","⬜️","️⬜️"]
+line3 = ["⬜️️","⬜️️","⬜️️"]
+map = [line1, line2, line3]
+print("Hiding your treasure! X marks the spot.")
+print(f"{column}\n{line1}\n{line2}\n{line3}")
+position = input("where do you want to hide the treasure?")
+
