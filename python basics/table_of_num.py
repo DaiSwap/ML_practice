3bab6bc5d5c1b628df51234a46a870df0ebfac38
DaiSwap <154792629+DaiSwap@users.noreply.github.com>
2024-09-24 11:48:29 +0530

    object not iterable

--- python basics/table_of_num.py
+++ python basics/table_of_num.py
@@ -1,21 +1,23 @@
 #given num n , print table of the num
-#approach3
+#approach3 , incase user asked specefic num of multiples
 n = int(input())
+m = int(input("num of multiples: "))
 i = 1
-while i<11:
+while i<=m:
     tab = i*n
     i = 1+i 
     print(tab)# a good practice to include print before changing i condition
 
 #my approach 1
-l = [1,2,3,4,5,6,7,8,9,10]
+l = int(input("num of multiples: "))
 n = int(input())
 for i in l:
-    l2 = n*i
+    l2 = n*i #error - object is not iterable because l is a int and it is not iterable like list or range
     print(l2)
 #approach 2
 n = int(input())
-for i in range(1,11):
+l = int(input("enter num of multiples"))
+for i in range(1,l+1):
     tab = n*i
     print(tab)  
       
