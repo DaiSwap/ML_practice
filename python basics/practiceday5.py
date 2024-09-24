5d79a17e46a7bda73d4d62a76454c21fc8e092fe
DaiSwap <154792629+DaiSwap@users.noreply.github.com>
2024-09-24 09:58:31 +0530

    iterating over loops

---
+++ python basics/practiceday5.py
@@ -0,0 +1,12 @@
+for x in range(20):
+    if x%6==0:
+        print(x)
+#iterating over a sequence , loops.
+l = [10,20,30,40]
+for i in range(len(l)):
+    print(l[i])
+#gives an item , its index and its position - below code
+l = [10,20,30,40]
+for i in range(len(l)):
+    print(i,l[i])
+            
