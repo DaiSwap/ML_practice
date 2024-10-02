b083169db62985c8a552e643ba7ee1ff7cbcd64e
DaiSwap <154792629+DaiSwap@users.noreply.github.com>
2024-10-02 22:55:55 +0530

    creating a get route - path parameter

--- API_setup/api_setup.py
+++ API_setup/api_setup.py
@@ -1,10 +1,30 @@
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-
+"""
 @app.route("/")
 def home():
     return "Home"
+    """
 #Creating first root inside of flask
+
+#GET request data from a specified source 
+#POST Create a resource
+#PUT update a resource
+#DELETE delete a resource
+
+#cretating get route - path parameter (a dynamic value passing in url)
+
+@app.route("/get-user/<user_id>")
+def get_user(user_id):
+    user_data = {
+        "user_id": user_id,
+        "name":"DaiSwap",
+        "email": "daiswap@exampe.com"
+    }
+#query parameter     
+    
+    
+
 if __name__ == "__main__":
     app.run(debug=True)
