2219daee1023b0569aa1f02b97fe1ada21128903
DaiSwap <154792629+DaiSwap@users.noreply.github.com>
2024-10-11 14:31:28 +0530

    max_score

--- python basics/practiceday5_2.py
+++ python basics/practiceday5_2.py
@@ -1,4 +1,9 @@
-fruits = ['applie','peach','banana']
-for fruit in fruits:
-    print(fruit)
-    print(fruits + " pie ")
+#highest score
+student_scores = [190,180,124,134,156,178,167,145,150,190,199,179,180,123,145]
+print(max(student_scores))
+max_score = 0
+for score in student_scores:
+    if score>max_score:
+        max_score = score 
+print(max_score)        
+
