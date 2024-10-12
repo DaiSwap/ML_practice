3dd2bc7bd17c8d88070d48546dfde7b4f138eb20
DaiSwap <154792629+DaiSwap@users.noreply.github.com>
2024-10-12 12:07:12 +0530

    usage of while loop and reeborg hurdle2

--- python basics/practiceday6.py
+++ python basics/practiceday6.py
@@ -30,4 +30,22 @@ for step in range(0,6):
     
 
     
+#hurdle 2 link - https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
+# my solution -
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
 
+while at_goal()==False:
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+
+    
---
+++ python basics/practiceday6_1.py
@@ -0,0 +1,7 @@
+#while loop , will keep going until the condition is satisfied
+# while something_is_true:
+#     do something , will stop if False
+i = 4
+while i>3:
+    print(i)
+    break
