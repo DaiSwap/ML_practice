be616be112c546c945073bbe44a17640a94208a1
DaiSwap <154792629+DaiSwap@users.noreply.github.com>
2024-10-13 18:23:23 +0530

    realized strings are iterable as such , no need to convert to list

--- python basics/practiceday7.py
+++ python basics/practiceday7.py
@@ -4,8 +4,8 @@ wordlist = ['template','baboon','shirts','dictate','typhoon']
 
 chosen_word = random.choice(wordlist)
 print(chosen_word)
-chosen_word = list(chosen_word) # converting to list so can iterate over
-print(chosen_word) 
+#chosen_word = list(chosen_word) # converting to list so can iterate over
+#print(chosen_word) 
 guess = input("guess a letter").lower()
  #converting all to lowercase
 for word in chosen_word:
