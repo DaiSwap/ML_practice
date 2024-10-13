23b5e145870d500efc760badfe1473a62ceddf3b
DaiSwap <154792629+DaiSwap@users.noreply.github.com>
2024-10-13 18:19:54 +0530

    usage of .lower() correctly

--- python basics/practiceday7.py
+++ python basics/practiceday7.py
@@ -6,8 +6,8 @@ chosen_word = random.choice(wordlist)
 print(chosen_word)
 chosen_word = list(chosen_word) # converting to list so can iterate over
 print(chosen_word) 
-guess = input("guess a letter")
-guess=guess.lower() #converting all to lowercase
+guess = input("guess a letter").lower()
+ #converting all to lowercase
 for word in chosen_word:
     
     if guess == word:
