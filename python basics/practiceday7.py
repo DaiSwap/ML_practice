94b55aa7265928d7218a2674a2907e30e035b142
DaiSwap <154792629+DaiSwap@users.noreply.github.com>
2024-10-15 14:49:26 +0530

    mini hangman part1

--- python basics/practiceday7.py
+++ python basics/practiceday7.py
@@ -1,17 +1,22 @@
 #hangman part 1 and part 2
 import random
 wordlist = ['template','baboon','shirts','dictate','typhoon']
-
+display =[]
 chosen_word = random.choice(wordlist)
 print(chosen_word)
+
 #chosen_word = list(chosen_word) # converting to list so can iterate over
 #print(chosen_word) 
-guess = input("guess a letter").lower()
- #converting all to lowercase
+guess = input("guess the letter").lower()#converting all to lowercase
+
+
 for word in chosen_word:
     
-    if guess == word:
-        print("correct")
+    if word == guess:
+        display.append(word)
+        
     else:
-        print("wrong")
-       
+        display.append("_")
+print(display)
+
+  
