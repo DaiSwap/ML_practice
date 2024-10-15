f373391b5ad4ee14b29920030797745784ff8ccc
DaiSwap <154792629+DaiSwap@users.noreply.github.com>
2024-10-15 15:56:56 +0530

    usage of while & keeping variable list outside the loop and store the correct guess and reuse

--- python basics/practiceday7_1.py
+++ python basics/practiceday7_1.py
@@ -1,8 +1,27 @@
-#using ceil function
-import math
-x = 4.2
-y = 8.1
+import random
+word_list = ["apple","template","baboon"]
+chosen_word = random.choice(word_list)
+print(chosen_word)
+placeholder = ""
+for letter in chosen_word: #note - can also take len(chosen_word) and assign it to variable and use the range function
+    placeholder += " _"
+print(placeholder)    
 
-print(math.ceil(x))
-print(math.ceil(y))
+correct_letters = []
+game_over = False
+while not game_over:
+    guess = input("guess a letter- ").lower()
+    display = ""
+    for letter in chosen_word:
+        if letter == guess:
+            display = display+letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display+=letter
+        else:
+            display = display+"_ "
+    print(display)        
+    if "_" not in display:
+        game_over = True    
+    
 
