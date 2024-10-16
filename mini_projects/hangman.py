04c524a3d0f3e65754e8f2923e5dba2f64204e45
DaiSwap <154792629+DaiSwap@users.noreply.github.com>
2024-10-16 15:46:54 +0530

    mini hangman usage , reviewed concepts

--- mini_projects/hangman.py
+++ mini_projects/hangman.py
@@ -1,15 +1,19 @@
 import random
 
-hangman = ["wrong1","wrong2","wrong3","wrong4","wrong5","wrong6"]
+hangman = ["no more lives","1life","2lives","3lives","4lives","5lives","6lives"]
 word_list = ["baboon","tomato","camels","trades"]
 
 chosen_word = random.choice(word_list)
 #print(chosen_word)
 dash = ""
+
 for letter in chosen_word:
     dash+="_"
 print(dash)
+#making variable life to keep track of number of chances
+lives = 6
 correct_words = []
+
 game_over = False
 while not game_over:
     guess = input("guess a letter: ")
@@ -23,7 +27,17 @@ while not game_over:
             display+=letter
         else:
             display+="_"
+            
     print(display)
     if "_" not in display:
-        game_over = True        
+        game_over = True
+        print("you win")
+       
+    if guess not in chosen_word:
+        lives = lives -1
+        
+        if lives ==0:
+            game_over=True
+            print(f"you lose, it was {chosen_word}") 
+    print(hangman[lives])                
             
