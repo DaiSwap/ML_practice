eb6e98e760a05d26954d55d21fe8f5f40a1a99ac
DaiSwap <154792629+DaiSwap@users.noreply.github.com>
2024-10-17 22:53:31 +0530

    parameters and arguments

--- python basics/practiceday8.py
+++ python basics/practiceday8.py
@@ -7,7 +7,7 @@ greet()
 
 # function allowing for inputs
 
-def greet_with_name(name):
+def greet_with_name(name):#name is parameter
     print(f"welcome {name}")
     print(f"ceasor cypher welcomes {name}")
-greet_with_name("pv")    # one function taking different inputs
+greet_with_name("pv")    # argument
