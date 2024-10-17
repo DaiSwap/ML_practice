1e285b59823f29df2ac9fc64c2d5a8a80172c401
DaiSwap <154792629+DaiSwap@users.noreply.github.com>
2024-10-17 22:52:27 +0530

    function with inputs

---
+++ python basics/practiceday8.py
@@ -0,0 +1,13 @@
+# simple function
+def greet():
+    print("welcome")
+    print("ceasor cypher")
+    
+greet()
+
+# function allowing for inputs
+
+def greet_with_name(name):
+    print(f"welcome {name}")
+    print(f"ceasor cypher welcomes {name}")
+greet_with_name("pv")    # one function taking different inputs
