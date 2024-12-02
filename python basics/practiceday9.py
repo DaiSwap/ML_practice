fcaccdc843dd8575564f8d1e68406411aac395c6
DaiSwap <154792629+DaiSwap@users.noreply.github.com>
2024-12-02 13:06:02 +0530

    practice problem for problem statement class & methods

---
+++ python basics/practiceday9.py
@@ -0,0 +1,18 @@
+#Create student class taking name and marks of 3 subjects as arguments in constructors.
+#then create method to print the average
+
+class Student:
+    def __init__(self,name,marks):
+        self.name = name
+        self.marks = marks
+        
+    def get_avg(self):
+        sum = 0
+        for val in self.marks:
+            sum = sum + val
+            
+        print(sum/3)
+        
+s1 = Student("pv",[24,21,22])
+print("the avg marks of",s1.name,"is") ; s1.get_avg()        
+               
